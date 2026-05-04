cda400c2243f6d4627c4204d8d225d022eecef81
tyshushi <tzeyuh@gmail.com>
2026-05-04 13:13:35 +0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/perftrack-pms/',
   server: {
     host: true,
     port: 5173,
-    proxy: {
-      '/api': { target: 'http://backend:8000', changeOrigin: true }
-    }
   }
 })
